8e913724a5187666fc3e5cc17f3b9e10555e779a
RachaponKjr <rachapon.navy@gmail.com>
2025-12-31 05:36:32 +0700

    fix

--- yoka-api/src/app.ts
+++ yoka-api/src/app.ts
@@ -28,7 +28,7 @@ app.use(
   cors({
     origin: [
       "http://119.59.99.141:3001", // IP จริงของ Frontend
-      "http://localhost:3001", // เผื่อเทส Local
+      "http://localhost:3000", // เผื่อเทส Local
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
