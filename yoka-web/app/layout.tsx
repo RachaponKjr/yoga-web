fd239aa576c25b7edcdc2b03f883b6a8fbb8531d
RachaponKjr <rachapon.navy@gmail.com>
2026-01-16 20:41:03 +0700

    update

--- yoka-web/app/layout.tsx
+++ yoka-web/app/layout.tsx
@@ -47,13 +47,14 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-secondary!`}
       >
         <TanstackProvider>
-          <SmoothScrolling />
-          <AuthHydrator user={user} />
-          <Navbar />
-          {children}
-          <ScrollTop />
-          <Toaster />
-          <Footer />
+          <SmoothScrolling>
+            <AuthHydrator user={user} />
+            <Navbar />
+            {children}
+            <ScrollTop />
+            <Toaster />
+            <Footer />
+          </SmoothScrolling>
         </TanstackProvider>
       </body>
     </html>
