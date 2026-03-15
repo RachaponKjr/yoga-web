3e0c934504d4df4daca3e7967d140e457257118f
RachaponKjr <rachapon.navy@gmail.com>
2026-03-15 11:16:35 +0700

    update

--- yoka-api/src/config/omise.ts
+++ yoka-api/src/config/omise.ts
@@ -3,10 +3,8 @@ import Omise from "omise";
 import { config } from "./env"; // ไฟล์ config env ของคุณ
 
 const omise = Omise({
-  // publicKey: process.env.OMISE_PUBLIC_KEY as string,
-  // secretKey: process.env.OMISE_SECRET_KEY as string,
-  publicKey: "pkey_test_62d7zsjee4rw6f5ckij" as string,
-  secretKey: "skey_test_62d7zsjrrkakbo2ees3" as string,
+  publicKey: process.env.OMISE_PUBLIC_KEY as string,
+  secretKey: process.env.OMISE_SECRET_KEY as string,
 });
 
 export default omise;
